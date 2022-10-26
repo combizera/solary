d0866a268e3d509593861fd8494d05d002891536
Ygor Combi <ycombi349@gmail.com>
2022-10-26 20:26:05 -0300

    nova pagina 404

--- README.md
+++ README.md
@@ -15,3 +15,137 @@ Site que mostra alguns dos meus principais projetos pessoais, assim como também
 Títulos e destaques
 Serifada - Amiri
 Regular e Bold
+
+## Elementos não usados
+
+--Footer antigo
+
+---HTML
+
+<footer>
+  <div class="footer-bg container">
+    <div class="footer-logo">
+      <a href="#"><img src="./assets/img/icon/svgs/logo-footer.svg" alt="Logotipo Solary"></a>
+    </div>
+    <div class="footer-left">
+      <div class="footer-contato">
+        <p class="footer-tit">Contato</p>
+        <a href="mailto:ola@solary.com.br"> ola@solary.com.br</a>
+      </div>
+      <div class="footer-sociais">
+        <p class="footer-tit">Sociais</p>
+        <a href="#"><img src="./assets/img/icon/svgs/soc-zap.svg" alt=""></a>
+        <a href="#"><img src="./assets/img/icon/svgs/soc-ig.svg" alt=""></a>
+        <a href="#"><img src="./assets/img/icon/svgs/soc-be.svg" alt=""></a>
+      </div>
+    </div>
+
+    <div class="footer-right">
+      <p class="footer-tit">Assine a newsletter</p>
+      <p></p>
+      <div class="footer-form">
+        <form action="POST" action="./">
+          <label for="news-label">Prometemos não enviar spam :p </label>
+          <div class="news-bloco">
+            <input id="news-email" type="email" name="news-email" placeholder="Insira aqui seu melhor e-mail" required>
+            <button id="btn-insc">Inscrever</button>
+        </form>
+      </div>
+    </div>
+
+  </div>
+</footer>
+---CSS
+footer {
+  background-color: #404040;
+  font-family: "Saira", sans-serif;
+}
+footer a,
+label {
+  font-weight: 300;
+  color: #b0b0b0;
+  transition: 0.3s;
+}
+
+#news-label {
+line-height: 100%;
+}
+
+footer a:hover {
+color: var(--bg);
+}
+
+.footer-bg {
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+padding: 60px 0;
+}
+
+.footer-left {
+display: flex;
+justify-content: space-between;
+}
+.footer-tit {
+font-weight: 700;
+font-size: 1.5rem;
+line-height: 40px;
+color: var(--bg);
+}
+.footer-logo {
+display: flex;
+align-items: center;
+justify-content: center;
+}
+
+.footer-logo img {
+max-width: 100px;
+}
+
+.footer-sociais:last-child {
+margin-right: 0rem;
+}
+
+.footer-sociais a {
+margin-right: 1rem;
+}
+
+/_ Formulário _/
+.news-bloco {
+position: relative;
+}
+
+input {
+background-color: #ebe7e1;
+width: 100%;
+border-radius: 16px;
+padding: 0.45rem 1rem;
+margin: 0.5rem 0;
+border: none;
+
+font-family: "Saira";
+font-size: 14px;
+
+color: #abaaaa;
+}
+
+button {
+cursor: pointer;
+position: absolute;
+top: 0;
+margin: 0.5rem 0;
+right: 0;
+font-family: "Saira", sans-serif;
+
+text-transform: uppercase;
+padding: 0.5rem 1rem;
+border-left: 0px;
+border: none;
+transition: 0.3s;
+font-weight: 700;
+line-height: 150%;
+letter-spacing: 0.1em;
+
+color: #ebe7e1;
+background: linear-gradient(180deg, #7f62f4 0%, #7f44f4 100%);
+border-radius: 0px 16px 16px 0px;
+}
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   tl
     // cabecalho
-    .from(".logo", {
+    .from(".header-1", {
       duration: 1,
       opacity: 0,
       y: 50,
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "power3.out",
     });
   tl.from(
-    ".header1",
+    ".header-2",
     {
       duration: 1,
       opacity: 0,
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "-=0.8"
   );
   tl.from(
-    ".header2",
+    ".header-logo",
     {
       duration: 1,
       opacity: 0,
@@ -31,17 +31,17 @@ document.addEventListener("DOMContentLoaded", () => {
     "-=0.8"
   );
   tl.from(
-    ".header3",
+    ".header-3",
     {
       duration: 1,
       opacity: 0,
       ease: "power3.out",
       y: 50,
     },
-    "-=0.8"
+    "-=0.5"
   );
   tl.from(
-    ".header4",
+    ".header-4",
     {
       duration: 1,
       opacity: 0,
@@ -93,7 +93,6 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     "-=0.8"
   );
-  // antes do bg__hero é preciso mexer naquela imagem
   tl.from(
     ".bg__hero",
     {
@@ -129,6 +128,62 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  // Diferenciais
+  gsap.from("#diferenciais h4", {
+    duration: 2,
+    delay: 0.2,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#diferenciais ",
+    },
+  });
+
+  gsap.from("#diferenciais__responsivo .diferenciais__txt", {
+    duration: 1,
+    delay: 1,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#diferenciais__responsivo",
+    },
+  });
+
+  gsap.from("#diferenciais__responsivo .diferenciais__img", {
+    duration: 1,
+    delay: 1.4,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#diferenciais__responsivo",
+    },
+  });
+
+  gsap.from("#diferenciais__codigo .diferenciais__txt", {
+    duration: 1,
+    delay: 1,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#diferenciais__codigo",
+    },
+  });
+
+  gsap.from("#diferenciais__codigo .diferenciais__img", {
+    duration: 1,
+    delay: 1.4,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#diferenciais__codigo",
+    },
+  });
+
   // Números
   gsap.registerPlugin(ScrollTrigger);
   gsap.from("#numeros", {
@@ -154,9 +209,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Portfólio
   gsap.registerPlugin(ScrollTrigger);
-  gsap.from("#portfolio .sec__tit", {
-    duration: 1.5,
-    delay: 1,
+  gsap.from("#portfolio h2", {
+    duration: 1,
     y: 300,
     opacity: 0,
     ease: "power3.out",
@@ -165,9 +219,9 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  gsap.from(".port__itens div", {
+  gsap.from("#portfolio p", {
     duration: 1,
-    delay: 2,
+    delay: 0.3,
     y: 300,
     opacity: 0,
     stagger: { each: 0.3 },
@@ -177,50 +231,141 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  // Feedback
+  gsap.from("#portfolio .port-item1", {
+    duration: 1,
+    delay: 1,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#portfolio .port-grid",
+    },
+  });
+
+  gsap.from("#portfolio .port-item2", {
+    duration: 1,
+    delay: 1.3,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#portfolio .port-grid",
+    },
+  });
+
+  gsap.from("#portfolio .port-item3", {
+    duration: 1,
+    delay: 0.2,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#portfolio .port-item3",
+    },
+  });
+
+  gsap.from("#portfolio .port-item4", {
+    duration: 1,
+    delay: 0.6,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#portfolio .port-item3",
+    },
+  });
+
+  // Depoimentos
   gsap.registerPlugin(ScrollTrigger);
-  gsap.from(".feed__img", {
-    duration: 1.5,
+  gsap.from(".depo h4", {
+    duration: 1,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".depo",
+    },
+  });
+
+  gsap.from(".depo-main", {
+    duration: 1,
+    delay: 0.3,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".depo-main",
+    },
+  });
+
+  gsap.from(".swiper-slide img", {
+    duration: 1,
     delay: 0.5,
-
-    x: -600,
-    opacity: 0,
-    ease: "power3.out",
-    scrollTrigger: {
-      trigger: "#feedback",
-    },
-  });
-
-  gsap.from(".feed__tit", {
-    duration: 1,
-    delay: 0.7,
     y: 300,
     opacity: 0,
     ease: "power3.out",
     scrollTrigger: {
-      trigger: "#feedback",
+      trigger: ".depo-main",
     },
   });
 
-  gsap.from(".feed__depo", {
+  gsap.from(".swiper-slide .depo-txt", {
     duration: 1,
-    delay: 0.9,
+    delay: 1,
+    y: 300,
+    opacity: 0,
+    stagger: { each: 0.3 },
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".depo-main",
+    },
+  });
+
+  // Footer
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.from(".footer-logo", {
+    duration: 1,
     y: 300,
     opacity: 0,
     ease: "power3.out",
     scrollTrigger: {
-      trigger: "#feedback",
+      trigger: "footer",
     },
   });
-
-  gsap.from(".feed__pessoa", {
+  gsap.from(".footer-email", {
     duration: 1,
-    delay: 1.1,
+    delay: 0.4,
     y: 300,
     opacity: 0,
     ease: "power3.out",
     scrollTrigger: {
-      trigger: "#feedback",
+      trigger: "footer",
+    },
+  });
+  gsap.from(".footer-social img", {
+    duration: 1,
+    delay: 0.4,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    stagger: { each: 0.3 },
+    scrollTrigger: {
+      trigger: "footer",
+    },
+  });
+  gsap.from(".footer-cnpj", {
+    duration: 1,
+    delay: 1.4,
+    y: 300,
+    opacity: 0,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "footer",
     },
   });
 });
