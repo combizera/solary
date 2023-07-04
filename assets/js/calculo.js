58e44b6e75940ee1be9307cc4a86d9f17cc45896
Ygor Combi <ycombi349@gmail.com>
2023-07-03 23:29:26 -0300

    FEAT: seção calculo

---
+++ assets/js/calculo.js
@@ -0,0 +1,17 @@
+function calcular() {
+  var pontos = parseInt(document.getElementById('pontos').value);
+  var valor = 0;
+
+  if (pontos <= 3000) {
+    valor = pontos * 0.20;
+  } else if (pontos <= 9000) {
+    valor = 600 + (pontos - 3000) * 0.30;
+  } else if (pontos <= 15000) {
+    valor = 2400 + (pontos - 9000) * 0.10;
+  } else {
+    var excedente = pontos - 15000;
+    valor = 2400 + 600 + 600 + Math.floor(excedente / 1000) * 50;
+  }
+
+  document.getElementById('resultado').textContent = 'Valor a receber: R$ ' + valor.toFixed(2);
+}
