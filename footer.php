322ab540926425b650106ccbf4cc802d629f416c
Ygor Combi <ycombi349@gmail.com>
2023-07-08 07:50:35 -0300

    REFACTOR: header and footer in php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!-- FOOTER -->
+<footer class="container">
+  <div class="footer-logo"><img src="./assets/img/icon/log.svg" alt="Ícone Solary"></div>
+  <div class="footer-dados">
+    <a href="mailto:ola@agenciasolary.com.br" class="footer-email">ola@agenciasolary.com.br</a>
+    <div class="footer-social">
+      <a target="_blank" href="https://wa.me/5553999902079"> <img loading="lazy" src="./assets/img/icon/svgs/soc-zap.svg" alt="Whatsapp da Solary"></a>
+      <a target="_blank" href="https://instagram.com/agenciasolary"> <img loading="lazy" src="./assets/img/icon/svgs/soc-ig.svg" alt="Instagram da Solary"></a>
+      <a target="_blank" href="https://www.behance.net/combizera"> <img loading="lazy" src="./assets/img/icon/svgs/soc-be.svg" alt="Behance da Solary" id="behance"></a>
+    </div>
+    <p class="footer-cnpj">CNPJ: 47.409.055/0001-13</p>
+  </div>
+</footer>
+
+<!-- Bib. GSAP -->
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/gsap.min.js" integrity="sha512-GQ5/eIhs41UXpG6fGo4xQBpwSEj9RrBvMuKyE2h/2vw3a9x85T1Bt0JglOUVJJLeyIUl/S/kCdDXlE/n7zCjIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Bib. ScrollTrigger -->
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/ScrollTrigger.min.js" integrity="sha512-6BvDODEWgjWWyBrg6YFio6xmzgKWpf54tDlnCtG05m++/jgh/7jQcx6jUJrq44lB84cf68FzYiT0LipabA5g8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- MEUS CÓDIGOS JS -->
+<script src="assets/js/script.js"></script>
+<script defer src="assets/js/swiper.js"></script>
+
+<!-- FOOTER WORDPRESS -->
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <!-- Orgulhosamente desenvolvido por agenciasolary.com.br -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Folha de estilos -->
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+
+  <!-- Google tag (gtag.js) -->
+  <script defer src="https://www.googletagmanager.com/gtag/js?id=G-QP9PL9SE5C"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-QP9PL9SE5C');
+  </script>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+  <!-- Descrição -->
+  <meta name="description" content="Construímos produtos intuitivos que atendem às necessidades dos usuários. Projetamos do 0, não utilizamos layouts prontos, cada produto é único e completo.">
+
+  <!-- Fontes usadas -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@400;700&family=Poppins:wght@300;700&family=Saira:wght@300;500;700&display=swap" rel="stylesheet">
+
+  <!-- Pacote Swiper -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+  <script defer src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+  <!-- Título -->
+  <title>Agência Solary</title>
+
+  <!-- HEADER WORDPRESS -->
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <!-- HEADER -->
+  <header class="container">
+    <div class="header-bloco">
+      <div class="header-left">
+        <a href="#metodologia">
+          <p class="header-1">metodologia</p>
+        </a>
+        <a href="#diferenciais">
+          <p class="header-2">diferenciais</p>
+        </a>
+      </div>
+      <div class="header-logo">
+        <a href="#">
+          <img src="./assets/img/icon/log.svg" alt="Ícone Solary"></a>
+      </div>
+      <div class="header-right">
+        <a target="_blank" href="https://forms.gle/vWMG9GMsxZWfwRJ88">
+          <p class="header-3">formulário</p>
+        </a>
+        <a target="_blank" href="https://wa.me/5553999902079">
+          <p class="header-4">contato</p>
+        </a>
+      </div>
+    </div>
+  </header>
